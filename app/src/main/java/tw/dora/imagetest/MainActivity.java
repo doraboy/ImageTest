04dd51e45997226026a9603fbe8c203b02ea9f03
kleinerQ <chih.yen5408@gmail.com>
2018-10-20 10:15:26 +0800

    v2

--- app/src/main/java/tw/dora/imagetest/MainActivity.java
+++ app/src/main/java/tw/dora/imagetest/MainActivity.java
@@ -2,12 +2,40 @@ package tw.dora.imagetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imgView;
+    private int imgW, imgH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.imgView);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int vW = dm.widthPixels;
+        int vH = dm.heightPixels;
+        float dd = dm.density;
+        Log.v("brad","Window: "+vW+" x "+vH);
+        Log.v("brad","Window Density: "+dd);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v("brad","onStart");
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        imgW  = imgView.getWidth();
+        imgH = imgView.getHeight();
+        Log.v("brad",imgW+" x "+imgH);
     }
 }
